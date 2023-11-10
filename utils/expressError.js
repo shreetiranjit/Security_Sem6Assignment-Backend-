d7c6aae299db97303ebf59c4809be19f88c1e6dd
shreetiranjit <ranjit.shreeti1@gmail.com>
2023-11-10 09:42:44 +0545

    setted up expressError class to handle error in entire system. made final changes in the codee as well

--- utils/expressError.js
+++ utils/expressError.js
@@ -1,9 +1,10 @@
+//setting up expressError class to handle errors
 class expressError extends Error {
   constructor(message, statusCode) {
-    super();
-    this.message = message;
-    this.statusCode = statusCode;
+    super()
+    this.message = message
+    this.statusCode = statusCode
   }
 }
 
-module.exports = expressError;
+module.exports = expressError
